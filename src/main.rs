9bd60e58e9535f684def3336e29f8a9f6cd7b0f6
Sxlly <shaesullivan.buisness1@gmail.com>
2024-09-17 18:58:23 +0800

    integer example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    
+    //primitive datatypes: scalar and compound
+
+    //scalar datatypes
+
+    //integer
+
+    let x = 2; //impicitly typed "i32" by compiler
+    let x: i32 = 2; //explicity defined type to integer32 by "us" the programmer
+
+
+
 }
