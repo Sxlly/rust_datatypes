e9f3abecc91772b8983fc8081372e177366f158b
Sxlly <shaesullivan.buisness1@gmail.com>
2024-09-17 18:59:17 +0800

    comments

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 
     //integer
 
-    let x = 2; //impicitly typed "i32" by compiler
+    let x = 2; //impicitly typed "i32" by compiler **i32 is also the default datatype of integer in rust although you can sign anywhere from i8-128
     let x: i32 = 2; //explicity defined type to integer32 by "us" the programmer
 
 
