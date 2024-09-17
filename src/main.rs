bc44b60fb7817910cb1cb46b8788ab4e9c7f9ce4
Sxlly <shaesullivan.buisness1@gmail.com>
2024-09-17 19:00:43 +0800

    unsigned int example

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ fn main() {
     let x = 2; //impicitly typed "i32" by compiler **i32 is also the default datatype of integer in rust although you can sign anywhere from i8-128
     let x: i32 = 2; //explicity defined type to integer32 by "us" the programmer
 
+    let x: u32 = 400; //unsigned integer datatype-- this cannot have a sign attached to its value like "-30430" for example
+
 
 
 }
